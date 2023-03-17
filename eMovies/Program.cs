765cb7a6632e2e34217dce36b922c4fa93050513
Danilo Siervi <danilosiervidepaula16@gmail.com>
2023-03-17 14:29:18 -0300

    Configuring the sqlserver

--- eMovies/Program.cs
+++ eMovies/Program.cs
@@ -1,11 +1,15 @@
 using eMovies.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
