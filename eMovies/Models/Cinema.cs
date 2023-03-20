55fc184159e6719c9ea1fde626ebd62871a451c9
Danilo Siervi <danilosiervidepaula16@gmail.com>
2023-03-20 13:30:25 -0300

    Adding the Cinemas and Directors view

--- eMovies/Models/Cinema.cs
+++ eMovies/Models/Cinema.cs
@@ -7,8 +7,11 @@ public class Cinema
     [Key]
     public int Id { get; set; }
 
+    [Display(Name = "Cinema Name")]
     public string Name { get; set; }
+    [Display(Name = "Cinema Logo")]
     public string Logo { get; set; }
+    [Display(Name = "Cinema Description")]
     public string Description { get; set; }
 
     public ICollection<Movie> Movies { get; set; }
--- eMovies/Models/Director.cs
+++ eMovies/Models/Director.cs
@@ -7,8 +7,11 @@ public class Director
     [Key]
     public int Id { get; set; }
 
+    [Display(Name = "Full Name")]
     public string Name { get; set; }
+    [Display(Name = "Profile Picture")]
     public string ProfilePictureURL { get; set; }
+    [Display(Name = "Biography")]
     public string Bio { get; set; }
 
     public ICollection<Movie> Movies { get; set; }
